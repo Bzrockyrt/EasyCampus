c80fad2e224d48cc9039d0381ede660182d6946f
Hugoood <roueche.hugo@hotmail.fr>
2023-03-14 11:55:51 +0100

    SidebarComponent

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,7 @@
+import { Flex } from '@chakra-ui/react';
+import React from 'react';
+
+export default function SideBar() {
+
+    return <Flex width="256px" height="100vh" display="fixed" backgroundColor="pink"></Flex>
+}
