7e0e3b5aa3a78872f51933bd9521abc28c5bb3b0
Hugoood <roueche.hugo@hotmail.fr>
2023-03-14 16:27:13 +0100

    Sidebar  => Navbar

--- src/components/sidebar.tsx
+++
@@ -1,7 +0,0 @@
-import { Flex } from '@chakra-ui/react';
-import React from 'react';
-
-export default function SideBar() {
-
-    return <Flex width="256px" height="100vh" display="fixed" backgroundColor="pink"></Flex>
-}
