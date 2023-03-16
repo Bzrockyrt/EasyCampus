80e14405adfafa7b4b7e7f837ce19de93de3d078
Hugoood <roueche.hugo@hotmail.fr>
2023-03-16 13:48:19 +0100

    fixed signIn.jsx

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,39 +1,23 @@
-import React, { useState } from "react";
-import { useOutletContext } from 'react-router-dom'
 import { Flex } from "@chakra-ui/react";
 import { signInWithEmailAndPassword } from "firebase/auth";
-<<<<<<< HEAD:src/SignIn.jsx
-import { auth } from "./firebase";
-import { useNavigate } from "react-router-dom"
-=======
 import React, { useEffect, useState } from "react";
 import { auth } from "../../firebase";
 import './style/SignIn.css'
->>>>>>> register:src/pages/SignIn/SignIn.jsx
 
 export default function SignIn() {
-    const [user, setUser] = useOutletContext()
-    console.log('user', user)
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const navigate = useNavigate();
 
     const signIn = (e) => {
         e.preventDefault();
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 console.log(userCredential);
-<<<<<<< HEAD:src/SignIn.jsx
-                setUser(userCredential)
-                navigate('/profile')
-=======
                 // window.localStorage.setItem('uid', JSON.stringify(userCredential.user.uid))
->>>>>>> register:src/pages/SignIn/SignIn.jsx
             })
             .catch((error) => {
                 console.log(error);
             });
-
     };
 
     return (
