e86789c93501812c7790417bc2ee18bcbc2f0c11
ippolitp <pierrelouisippoliti@gmail.com>
2023-06-05 17:25:16 +0200

    dev presentation page lesson

--- src/pages/Lesson/Lesson.jsx
+++ src/pages/Lesson/Lesson.jsx
@@ -3,6 +3,17 @@ import {useLocation} from 'react-router-dom';
 export default function Lesson() {
     const location = useLocation();
     return (
-        <div>{location.state.name}</div>
+        <div className='container'>
+            <div className='container-header'>
+                {/* Mettre ici le titre de la leçon avec la note et le nom de l'étudiant qui l'a propose */}
+                <h1></h1>
+            </div>
+            <div className='container-body'>
+
+            </div>
+            <div className='container-footer'>
+
+            </div>
+        </div>
     )
 }
