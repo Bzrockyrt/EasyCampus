609a988b20aa2b5b36eafbcccfc5fcbe8ad419c2
ippolitp <pierrelouisippoliti@gmail.com>
2023-06-27 14:18:00 +0200

    custom filter

--- src/components/FilterLesson/FilterLesson.jsx
+++ src/components/FilterLesson/FilterLesson.jsx
@@ -1,5 +1,9 @@
 import { RangeSlider, RangeSliderTrack, RangeSliderFilledTrack, RangeSliderThumb, useRangeSlider } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
+import { doc, getDoc, updateDoc, collection } from "firebase/firestore";
+import { auth, db } from "../../firebase";
+
+
 
 export default function FilterLesson(props){
 
@@ -9,7 +13,7 @@ export default function FilterLesson(props){
 
     useEffect(() => {
         const getLessonPriceRangeData = async () => {
-            const querySnapshot = await getDocs(collection(db, "Lessons"));
+            const querySnapshot = await getDoc(collection(db, "Lessons"));
             const lessons = []
             if (querySnapshot) {
                 querySnapshot.docs.forEach(async (lessonDoc, index) => {
@@ -39,17 +43,37 @@ export default function FilterLesson(props){
 
     return (
         <div className="filterLesson-container">
-            <select name='filterLesson-Lessontype' onChange={onFilterLessonChanged}>
-                <option selected disabled>Type de cours</option>
+            <div class="icon">
+                <i id="left" class="fa-solid fa-angle-left"></i>
+            </div>
+            <ul class="tabs-box">
+                <li class="tab">Coding</li>
+                <li class="tab active">JavaScript</li>
+                <li class="tab">Podcasts</li>
+                <li class="tab">Databases</li>
+                <li class="tab">Web Development</li>
+                <li class="tab">Unboxing</li>
+                <li class="tab">History</li>
+                <li class="tab">Programming</li>
+                <li class="tab">Gadgets</li>
+                <li class="tab">Algorithms</li>
+                <li class="tab">Comedy</li>
+                <li class="tab">Gaming</li>
+                <li class="tab">Share Market</li>
+                <li class="tab">Smartphones</li>
+                <li class="tab">Data Structure</li>
+            </ul>
+            <div class="icon">
+                <i id="right" class="fa-solid fa-angle-right"></i>
+            </div>
+            {/* <select name='filterLesson-Lessontype' onChange={onFilterLessonChanged}>
+                <option defaultValue disabled>Type de cours</option>
                 <option value={'all'}>Tous</option>
                     {
                         props.tabMatieresData && props.tabMatieresData.map((matiere, i) => {
                             return <option value={matiere.id}>{matiere.nom}</option>
                         })
                     }
-                {/* <option value={"all"}>Tous</option>
-                <option value={"physic"}>Physique</option>
-                <option value={"french"}>Français</option> */}
             </select>
             <p>Notes :</p>
             <RangeSlider min={0} max={5} step={0.1} onChange={(e) => rangeSliderValueChanged(e.target.value.min)}>
@@ -61,7 +85,7 @@ export default function FilterLesson(props){
             </RangeSlider>
             <p>{rangeSliderMinValue}</p>
             <p>/</p>
-            <p>{rangeSliderMaxValue}</p>
+            <p>{rangeSliderMaxValue}</p> */}
         </div>
     );
 }
