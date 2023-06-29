274d64d7bd371cd217b968faed33d462b2cabd54
Pierre-Louis IPPOLITI <pierrelouisippoliti@gmail.com>
2023-06-29 13:30:39 +0200

    k

--- src/components/FilterLesson/FilterLesson.jsx
+++ src/components/FilterLesson/FilterLesson.jsx
@@ -41,11 +41,7 @@ export default function FilterLesson(props) {
 
     return (
         <div className="filterLesson-container">
-<<<<<<< Updated upstream
-            <select onChange={onFilterLessonChanged}>
-=======
             <select name='filterLesson-Lessontype' onChange={onFilterLessonChanged}>
->>>>>>> Stashed changes
                 <option defaultValue disabled>Type de cours</option>
                 <option value={'all'}>Tous</option>
                 {
